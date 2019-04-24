0466442633cd5e3bfb0c2cbd117f66590759c128
lowitz <lovem@student.chalmers.se>
2019-04-24 16:28:49 +0200

    Added unit tests and better description

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # cfsd-logic-recorder
-This microservice provides the recorder handling for Lynx.
+This microservice provides the recorder handling for Lynx.  
 The microservice starts and stops the recorder and lables the recorded data correctly based on mission and time of recording.
 
 ### Build
-AMD64: docker build -f Dockerfile.amd64 -t chalmersfsd/cfsd-logic-recorder:v0.0.0 .
+AMD64: docker build -f Dockerfile.amd64 -t chalmersfsd/cfsd-logic-recorder:v0.0.1 .
+
+### Run
+See included docker-compose file.
+
+### Requirements
+The path to the start and stop script (recorder-up.sh and recorder-down.sh) has to be provided when running the container (see yml file) and the scripts have to be present on the host.  
+The yml file for the actual recording has to be present in the same directory as the scripts.
--- test/tests-logic-recorder.cpp
+++ test/tests-logic-recorder.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018  Björnborg Nguyen
+ * Copyright (C) 2018  Love Mowitz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,50 +26,45 @@
 // #include <string>
 // #include <vector>
 
-TEST_CASE("Test OxTSDecoder with empty payload.") {
-    // const std::string DATA;
+TEST_CASE("Test recording start with right knob settings") {
+    cluon::OD4Session od4{111};
 
-    // OxTSDecoder d;
-    // auto retVal = d.decode(DATA);
-//
-    // REQUIRE(!retVal.first);
+    Recorder recorder(od4, "/home/cfsd/scripts/", "/home/cfsd/recordings/");
+    recorder.m_asState = asState::AS_READY;
+    recorder.m_asMission = asMission::AMI_ACCELERATION;
+    recorder.m_knobRight = 12;
+    recorder.m_knobLeft = 12;
+
+    recorder.run();
+
+    REQUIRE(recorder.m_recordingState == true);
 }
 
-// TEST_CASE("Test OxTSDecoder with faulty payload.") {
-//     const std::string DATA{"Hello World"};
+TEST_CASE("Test recording start with wrong knob settings") {
+    cluon::OD4Session od4{111};
 
-//     OxTSDecoder d;
-//     auto retVal = d.decode(DATA);
+    Recorder recorder(od4, "/home/cfsd/scripts/", "/home/cfsd/recordings/");
+    recorder.m_asState = asState::AS_READY;
+    recorder.m_asMission = asMission::AMI_ACCELERATION;
+    recorder.m_knobRight = 11;
+    recorder.m_knobLeft = 12;
 
-//     REQUIRE(!retVal.first);
-// }
+    recorder.run();
 
-// TEST_CASE("Test OxTSDecoder with sample payload.") {
-//     std::vector<uint8_t> sample{
-//       0xe7, 0x9c, 0x95, 0x95, 0x08, 0x00, 0x7c, 0x0e,
-//       0x00, 0x06, 0x81, 0xfe, 0x45, 0x00, 0x00, 0xf4,
-//       0x00, 0x00, 0xaa, 0xff, 0xff, 0x04, 0xc2, 0x92,
-//       0xf2, 0x9e, 0x60, 0x0a, 0x35, 0xf0, 0x3f, 0x46,
-//       0x63, 0x83, 0x3b, 0x7c, 0x96, 0xcc, 0x3f, 0x23,
-//       0x5a, 0xd0, 0x42, 0x32, 0x00, 0x00, 0x05, 0x00,
-//       0x00, 0x2c, 0x00, 0x00, 0xeb, 0xae, 0xe0, 0x00,
-//       0x59, 0x00, 0xbe, 0x6b, 0xff, 0xe4, 0x1d, 0x01,
-//       0x00, 0x00, 0x00, 0xff, 0xff, 0x01, 0xff, 0xe4
-//     };
+    REQUIRE(recorder.m_recordingState == false);
+}
 
-    // const std::string DATA(reinterpret_cast<char*>(sample.data()), sample.size());
+TEST_CASE("Test recording stop") {
+    cluon::OD4Session od4{111};
 
-    // OxTSDecoder d;
-    // auto retVal = d.decode(DATA);
+    Recorder recorder(od4, "/home/cfsd/scripts/", "/home/cfsd/recordings/");
+    recorder.m_asState = asState::AS_OFF;
+    recorder.m_asMission = asMission::AMI_NONE;
+    recorder.m_recordingState = true;
+    recorder.m_knobRight = 12;
+    recorder.m_knobLeft = 12;
 
-    // REQUIRE(retVal.first);
+    recorder.run();
 
-    // auto msgs = retVal.second;
-    // opendlv::proxy::GeodeticWgs84Reading msg1 = msgs.first;
-    // opendlv::proxy::GeodeticHeadingReading msg2 = msgs.second;
-
-    // REQUIRE(58.037722605 == Approx(msg1.latitude()));
-    // REQUIRE(12.796579564 == Approx(msg1.longitude()));
-
-    // REQUIRE(2.1584727764 == Approx(msg2.northHeading()));
-// }
+    REQUIRE(recorder.m_recordingState == false);
+}
